5124eea29539362d7cf40a60ebe59c3b18c5365a
Vyankatesh <vyankateshnaghate1@gmailcom>
2026-06-17 11:47:49 +0000

    Initial calculator app with Dockerfile

---
+++ app.py
@@ -0,0 +1,16 @@
+num1 = 10
+num2 = 5
+
+print("Simple Calculator")
+print("-----------------")
+print(f"Numbers: {num1} and {num2}")
+print(f"Addition:   {num1 + num2}")
+print(f"Substraction:  {num1 - num2}")
+print(f"Multiplication:  {num1 * num2}")
+print(f"Division:  {num1 / num2}")
+
+print(f"Power: {num2 ** num2}")
+
+print("This Calculator is made by VyanKatesh")
+
+
