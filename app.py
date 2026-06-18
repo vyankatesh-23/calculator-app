defcfc8daef140b47507c76ba077062ae1dd7c0f
Vyankatesh <vyankateshnaghate1@gmail.com>
2026-06-18 05:16:19 +0000

    Added power function

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ print(f"Substraction:  {num1 - num2}")
 print(f"Multiplication:  {num1 * num2}")
 print(f"Division:  {num1 / num2}")
 
-print(f"Power: {num2 ** num2}")
+print(f"Power: {num1 ** num2}")
 
 print("This Calculator is made by VyanKatesh")
 
